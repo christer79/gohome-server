99263e58e13518a8cdb9c99e45e0b27e61eec094
Christer Eriksson <christer.eriksson@gmail.com>
2017-06-07 19:07:43 +0200

    Adding disks as argument

--- main.go
+++ main.go
@@ -7,13 +7,39 @@ import (
 	"github.com/christer79/gohome-server/disks"
 	"github.com/christer79/gohome-server/ip"
 	"github.com/christer79/gohome-server/web"
+	"github.com/urfave/cli/altsrc"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	fmt.Println("Filesystems")
-	disks := disks.Client{Filesystems: []string{"/", "/mnt/3000GB", "/mnt/25000GB","/mnt/BACKUP_OTHER","/mnt/BACKUP_PHOTOS"}}
-	disks.Write(os.Stdout)
-	ip := ip.Client{}
-	ip.Write(os.Stdout)
-	web.Client{Disks: disks}.Start("9000")
+
+	app := cli.NewApp()
+	app.Name = "gohome"
+	app.Usage = "Home server monitoring."
+
+	app.Flags = []cli.Flag{
+		altsrc.NewStringSliceFlag(cli.StringSliceFlag{
+			Name:  "disks",
+			Usage: "List of disks to monitor usage of",
+		}),
+		cli.StringFlag{
+			Name:  "config",
+			Value: "config.yml",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println(c.GlobalStringSlice("disks"))
+		disks := disks.Client{Filesystems: c.StringSlice("disks")}
+
+		disks.Write(os.Stdout)
+		ip := ip.Client{}
+		ip.Write(os.Stdout)
+
+		web.Client{Disks: disks, IP: ip}.Start("9000")
+		return nil
+	}
+
+	app.Before = altsrc.InitInputSourceWithContext(app.Flags, altsrc.NewYamlSourceFromFlagFunc("config"))
+	app.Run(os.Args)
 }
